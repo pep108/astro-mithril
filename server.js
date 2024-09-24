3f2374c1e7b2c156b1015b0d5140b2bd86be9ed8
YohYamasaki <y.yamasaki@tutanota.com>
2024-09-24 21:24:19 +0200

    Fix server side renderer

--- server.js
+++ server.js
@@ -4,40 +4,48 @@ import StaticHtml from './static-html.js';
 
 const slotName = (str) => str.trim().replace(/[-_]([a-z])/g, (_, w) => w.toUpperCase());
 
+function isClass(func) {
+	return typeof func === 'function' && /^class\s/.test(func.toString());
+}
+
 async function check(Component, props, children) {
 	// Note: Mithril can use function, object and class components so the best
-  // way to check if the Component is valid is to try rendering it
+	// way to check if the Component is valid is to try rendering it
 	let error = null;
 	let isMithrilComponent = false;
-
-  // all mithril components need a view method
-  if (props && !props.view) {
-    return false
-  }
+	if(!Component) return;
+	// all mithril components need a view method
+	if(isClass(Component)) {
+		const instance = new Component;
+		if(!instance.view) return false;
+	} else if (typeof Component === "function") {
+		if(!Object.hasOwn(Component({attrs: props}), "view")) return false;
+	} else {
+		if(!Component.view) return false;
+	}
 
 	function Tester(...args) {
 		try {
-      const vnode = m(Component, args)
-      // If mithril doesn't throw an error, we know it can render the Component
-      isMithrilComponent = true;
+			m(Component, args)
+			// If mithril doesn't throw an error, we know it can render the Component
+			isMithrilComponent = true;
 		} catch (err) {
 			error = err;
-      return false
+			return false
 		}
-
-    return m('div')
+		return { view() { return m('div') } }
 	}
 
-	await renderToStaticMarkup(Tester, props, children, {});
+	await renderToStaticMarkup(Tester, props, children);
 
 	if (error) {
 		throw error;
 	}
 
-  return isMithrilComponent;
+	return isMithrilComponent;
 }
 
-async function renderToStaticMarkup(Component, props, { default: children, ...slotted }, metadata) {
+async function renderToStaticMarkup(Component, props, { default: children, ...slotted }) {
 	delete props['class'];
 	const slots = {};
 	for (const [key, value] of Object.entries(slotted)) {
@@ -53,14 +61,10 @@ async function renderToStaticMarkup(Component, props, { default: children, ...sl
 	if (newChildren != null) {
 		newProps.children = m(StaticHtml, { value: newChildren });
 	}
-	
-  let html;
-  html = render.sync({
-    view: () => {
-      return m(Component, newProps, newProps.children)
-    }
-  })
-  return { html }
+
+	let html;
+	html = render.sync(m(Component, newProps, newProps.children));
+	return { html }
 }
 
 export default {
