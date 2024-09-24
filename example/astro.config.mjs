fd71c213c2e69f784f57283b2a3497963b672373
pep <pep@daomed.org>
2024-09-24 14:14:18 -0700

    updated example astro config to avoid "React is not defined" error

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mithril from 'astro-mithril';
+import { defineConfig } from 'astro/config'
+import mithril from 'astro-mithril'
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Mithril to support Mithril JSX components.
-	integrations: [mithril()],
-});
+  // Enable Mithril to support Mithril JSX components.
+  integrations: [mithril()],
+  vite: {
+    esbuild: {
+      jsx: "transform",
+      jsxFactory: "m",
+      jsxFragment: "'['",
+    }
+  }
+})
